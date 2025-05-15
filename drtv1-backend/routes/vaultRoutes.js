fafdb8dc20cc59be5071ed656d8463ad896dbbdf
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-05-14 21:53:27 -0700

    Update vaultRoutes.js

--- drtv1-backend/routes/vaultRoutes.js
+++ drtv1-backend/routes/vaultRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const vaultController = require('../controllers/vaultController');
 
 router.post('/purchase', vaultController.handleDRTPurchase);
-router.post('/redeem', vaultController.redeemSeTH);
+router.post('/redeem', vaultController.redeemSeth);
 
 module.exports = router;
