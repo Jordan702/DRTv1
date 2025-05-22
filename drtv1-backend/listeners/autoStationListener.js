6a66504f2dee0a245eb6c7f48a19357d8b818637
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-05-22 00:10:32 -0700

    Update autoStationListener.js

--- drtv1-backend/listeners/autoStationListener.js
+++ drtv1-backend/listeners/autoStationListener.js
@@ -3,8 +3,8 @@ const { ethers } = require("ethers");
 const { summarizeChannel, analyzeSentiment } = require("../services/autonet_ai");
 require("dotenv").config();
 
-const contractAddress = "YOUR_CONTRACT_ADDRESS";
-const contractABI = require("../../contracts/AutoStation.json");
+const contractAddress = "0xc16b6aBd3082446a74dbe1F2574C10711693A9ae";
+const contractABI = require("../abi/AutoStation.json");
 const provider = new ethers.WebSocketProvider(process.env.MAINNET_RPC_URL);
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
