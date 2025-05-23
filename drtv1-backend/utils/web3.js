ba5f7395ce65aa2fc99dd52fa7c83ae79500bcd1
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-05-22 22:32:13 -0700

    Create web3.js

---
+++ drtv1-backend/utils/web3.js
@@ -0,0 +1,9 @@
+const { ethers } = require('ethers');
+const vaultAbi = require('../abi/DRTVaultV3.json');
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+const vault = new ethers.Contract(process.env.VAULT_ADDRESS, vaultAbi, signer);
+
+module.exports = { provider, signer, vault, ethers };
