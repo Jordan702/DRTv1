6db735f4b958643600adb718f170f0ee6bee9703
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-05-23 01:42:36 -0700

    Update balanceController.js

--- drtv1-backend/controllers/balanceController.js
+++ drtv1-backend/controllers/balanceController.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const { ethers, provider } = require('../utils/web3');
-const tokenAbi = require('../abi/ERC20_abi.json'); // Standard ERC20 ABI
+const stokenAbi = require('../abi/sETH_abi.json'); // Standard ERC20 ABI
+const dtokenAbi = require('../abi/DRT_abi.json'); // Standard ERC20 ABI
+const ddtokenAbi = require('../abi/DRTv2_abi.json'); // Standard ERC20 ABI
 
-const sETH = new ethers.Contract(process.env.SETH_ADDRESS, tokenAbi, provider);
-const dRTv1 = new ethers.Contract(process.env.DRT_V1_CONTRACT_ADDRESS, tokenAbi, provider);
-const dRTv2 = new ethers.Contract(process.env.DRT_V2_CONTRACT_ADDRESS, tokenAbi, provider);
+const sETH = new ethers.Contract(process.env.SETH_ADDRESS, stokenAbi, provider);
+const dRTv1 = new ethers.Contract(process.env.DRT_V1_CONTRACT_ADDRESS, dtokenAbi, provider);
+const dRTv2 = new ethers.Contract(process.env.DRT_V2_CONTRACT_ADDRESS, ddtokenAbi, provider);
 
 exports.getBalances = async (req, res) => {
   try {
