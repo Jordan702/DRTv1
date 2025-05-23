1e1f681feeeef3d847200edafd575b9376ebb979
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-05-23 01:37:44 -0700

    Update web3.js

--- drtv1-backend/utils/web3.js
+++ drtv1-backend/utils/web3.js
@@ -3,7 +3,7 @@ const { ethers } = require('ethers');
 const vaultAbi = require('../abi/DRTVaultV3_abi.json');
 
 const provider = new ethers.JsonRpcProvider(process.env.MAINNET_RPC_URL);
-const signer = new ethers.Wallet(process.env.MINTERS_PRIVATE_KEY, provider);
+const signer = new ethers.Wallet(process.env.MINTER_PRIVATE_KEY, provider);
 
 const vault = new ethers.Contract(process.env.VAULT_ADDRESS, vaultAbi, signer);
 
