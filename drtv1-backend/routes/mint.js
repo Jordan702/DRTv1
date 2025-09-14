6ea517efed8e4d8c1c69f0661920170c94b01a9e
Jordan Schwartz <94415375+Jordan702@users.noreply.github.com>
2025-09-13 23:57:41 -0700

    Update mint.js

--- drtv1-backend/routes/mint.js
+++ drtv1-backend/routes/mint.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const { mintsETH, doBatchMint } = require("../controllers/MintController");
 
-// Single mint endpoint
+// Single mint endpoint for sETH
 router.post("/", mintsETH);
 
-// Batch mint endpoint
+// Batch mint endpoint for DRTv2
 router.post("/batch-mint", doBatchMint);
 
 module.exports = router;
